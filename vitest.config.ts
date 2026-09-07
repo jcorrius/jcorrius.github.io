cdeada87b037baa8c990cc0d716a9514512e0b56
Jesús Corrius <jcorrius@gmail.com>
2026-09-07 14:18:19 +0200

    test: add coverage configuration to vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,5 +5,11 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: './src/test/setup.ts',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+      include: ['src/**/*.ts'],
+      exclude: ['src/test/**', 'src/main.ts'],
+    },
   },
 });
